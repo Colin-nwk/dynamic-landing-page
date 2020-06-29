f2343bd674a26f7f8df016172273fa1b4830353e
Collins Nwoko C <63902016+Colin-nwk@users.noreply.github.com>
2020-06-29 23:02:36 +0100

    Add files via upload

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,8 +5,8 @@ const time = document.getElementById('time'),
       name =document.getElementById('name'),
       focus =document.getElementById('focus');
 
-//mine due to img link in js not working
-// const bg = document.querySelector('.bg')
+// options
+const showAmPm = true;
 
 
 // show time
@@ -23,7 +23,7 @@ function showTime(){
     hour = hour % 12 || 12 ;
 
     // output time
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
 
     setTimeout(showTime, 1000);
 }
@@ -39,7 +39,7 @@ function setBgGreet(){
 
     if(hour < 12){
         // morning
-        document.body.style.backgroundImage = "url('/dist/img/mountain1.jpg')";
+        document.body.style.backgroundImage = "url('/dist/img/main.jpg')";
         greeting.textContent = 'Good Morning';
         
         // bg.classList.add('morning');
@@ -57,8 +57,57 @@ function setBgGreet(){
     }
 }
 
+// get name
+function getName(){
+    if(localStorage.getItem('name')=== null){
+        name.textContent = '[enter name]';
+    }else{
+        name.textContent = localStorage.getItem('name');
+    }
+}
 
+// set name
+function setName(e){
+    if(e.type === 'keypress'){
+        // make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13){
+            localStorage.setItem('name', e.target.innerHTML);
+            name.blur();
+        }
+    }else {
+        localStorage.setItem('name', e.target.innerHTML);
+    }
+}
+// set focus
+function setFocus(e){
+    if(e.type === 'keypress'){
+        // make sure enter is pressed
+        if(e.which == 13 || e.keyCode == 13){
+            localStorage.setItem('focus', e.target.innerHTML);
+            focus.blur();
+        }
+    }else {
+        localStorage.setItem('focus', e.target.innerHTML);
+    }
+}
+
+// get focus
+function getFocus(){
+    if(localStorage.getItem('focus')=== null){
+        focus.textContent = '[enter your focus]';
+    }else{
+        focus.textContent = localStorage.getItem('focus');
+    }
+}
+
+// event listeners
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 
 // run
 showTime();
 setBgGreet();
+getName();
+getFocus();
